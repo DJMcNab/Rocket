asimwe1/rust_chatapp

commit b8ba7b855f28be1f17bd65b35dd8597d368369ff

Remove Session in favor of private cookies. New testing API.
Sessions
--------

This commit removes the `Session` type in favor of methods on the
`Cookies` types that allow for adding, removing, and getting private
(signed and encrypted) cookies. These methods provide a superset of
the functionality of `Session` while also being a minimal addition to
the existing API. They can be used to implement the previous `Session`
type as well as other forms of session storage. The new methods are:

  * Cookie::add_private(&mut self, Cookie)
  * Cookie::remove_private(&mut self, Cookie)
  * Cookie::get_private(&self, &str)

Resolves #20

Testing
-------

This commit removes the `rocket::testing` module. It adds the
`rocket::local` module which provides a `Client` type for local
dispatching of requests against a `Rocket` instance. This `local`
package subsumes the previous `testing` package.

Rocket Examples
---------------

The `forms`, `optional_result`, and `hello_alt_methods` examples have
been removed. The following example have been renamed:

  * extended_validation -> form_validation
  * hello_ranks -> ranking
  * from_request -> request_guard
  * hello_tls -> tls

Other Changes
-------------

This commit also includes the following smaller changes:

  * Config::{development, staging, production} constructors have been
    added for easier creation of default `Config` structures.
  * The `Config` type is exported from the root.
  * `Request` implements `Clone` and `Debug`.
  * `Request::new` is no longer exported.
  * A `Response::body_bytes` method was added to easily retrieve a
    response's body as a `Vec<u8>`.

lib/benches/format-routing.rs
@@ -13,8 +13,7 @@ fn post() -> &'static str { "post" }
 
 fn rocket() -> rocket::Rocket {
     let config = Config::new(Environment::Production).unwrap();
-    rocket::custom(config, false)
-        .mount("/", routes![get, post])
+    rocket::custom(config, false).mount("/", routes![get, post])
 }
 
 mod benches {
@@ -22,35 +21,34 @@ mod benches {
 
     use super::rocket;
     use self::test::Bencher;
-    use rocket::testing::MockRequest;
-    use rocket::http::Method::*;
+    use rocket::local::Client;
     use rocket::http::{Accept, ContentType};
 
     #[bench]
     fn accept_format(b: &mut Bencher) {
-        let rocket = rocket();
-        let mut request = MockRequest::new(Get, "/").header(Accept::JSON);
-        b.iter(|| { request.dispatch_with(&rocket); });
+        let client = Client::new(rocket()).unwrap();
+        let mut request = client.get("/").header(Accept::JSON);
+        b.iter(|| { request.mut_dispatch(); });
     }
 
     #[bench]
     fn wrong_accept_format(b: &mut Bencher) {
-        let rocket = rocket();
-        let mut request = MockRequest::new(Get, "/").header(Accept::HTML);
-        b.iter(|| { request.dispatch_with(&rocket); });
+        let client = Client::new(rocket()).unwrap();
+        let mut request = client.get("/").header(Accept::HTML);
+        b.iter(|| { request.mut_dispatch(); });
     }
 
     #[bench]
     fn content_type_format(b: &mut Bencher) {
-        let rocket = rocket();
-        let mut request = MockRequest::new(Post, "/").header(ContentType::JSON);
-        b.iter(|| { request.dispatch_with(&rocket); });
+        let client = Client::new(rocket()).unwrap();
+        let mut request = client.post("/").header(ContentType::JSON);
+        b.iter(|| { request.mut_dispatch(); });
     }
 
     #[bench]
     fn wrong_content_type_format(b: &mut Bencher) {
-        let rocket = rocket();
-        let mut request = MockRequest::new(Post, "/").header(ContentType::Plain);
-        b.iter(|| { request.dispatch_with(&rocket); });
+        let client = Client::new(rocket()).unwrap();
+        let mut request = client.post("/").header(ContentType::Plain);
+        b.iter(|| { request.mut_dispatch(); });
     }
 }

lib/benches/ranked-routing.rs
@@ -35,36 +35,35 @@ mod benches {
 
     use super::rocket;
     use self::test::Bencher;
-    use rocket::testing::MockRequest;
-    use rocket::http::Method::*;
+    use rocket::local::Client;
     use rocket::http::{Accept, ContentType};
 
     #[bench]
     fn accept_format(b: &mut Bencher) {
-        let rocket = rocket();
+        let client = Client::new(rocket()).unwrap();
         let mut requests = vec![];
-        requests.push(MockRequest::new(Get, "/").header(Accept::JSON));
-        requests.push(MockRequest::new(Get, "/").header(Accept::HTML));
-        requests.push(MockRequest::new(Get, "/").header(Accept::Plain));
+        requests.push(client.get("/").header(Accept::JSON));
+        requests.push(client.get("/").header(Accept::HTML));
+        requests.push(client.get("/").header(Accept::Plain));
 
         b.iter(|| {
             for request in requests.iter_mut() {
-                request.dispatch_with(&rocket);
+                request.mut_dispatch();
             }
         });
     }
 
     #[bench]
     fn content_type_format(b: &mut Bencher) {
-        let rocket = rocket();
+        let client = Client::new(rocket()).unwrap();
         let mut requests = vec![];
-        requests.push(MockRequest::new(Post, "/").header(ContentType::JSON));
-        requests.push(MockRequest::new(Post, "/").header(ContentType::HTML));
-        requests.push(MockRequest::new(Post, "/").header(ContentType::Plain));
+        requests.push(client.post("/").header(ContentType::JSON));
+        requests.push(client.post("/").header(ContentType::HTML));
+        requests.push(client.post("/").header(ContentType::Plain));
 
         b.iter(|| {
             for request in requests.iter_mut() {
-                request.dispatch_with(&rocket);
+                request.mut_dispatch();
             }
         });
     }

lib/benches/simple-routing.rs
@@ -49,82 +49,82 @@ mod benches {
 
     use super::{hello_world_rocket, rocket};
     use self::test::Bencher;
-    use rocket::testing::MockRequest;
+    use rocket::local::Client;
     use rocket::http::Method::*;
 
     #[bench]
     fn bench_hello_world(b: &mut Bencher) {
-        let rocket = hello_world_rocket();
-        let mut request = MockRequest::new(Get, "/");
+        let client = Client::new(hello_world_rocket()).unwrap();
+        let mut request = client.get("/");
 
         b.iter(|| {
-            request.dispatch_with(&rocket);
+            request.mut_dispatch();
         });
     }
 
     #[bench]
     fn bench_single_get_index(b: &mut Bencher) {
-        let rocket = rocket();
-        let mut request = MockRequest::new(Get, "/");
+        let client = Client::new(rocket()).unwrap();
+        let mut request = client.get("/");
 
         b.iter(|| {
-            request.dispatch_with(&rocket);
+            request.mut_dispatch();
         });
     }
 
     #[bench]
     fn bench_get_put_post_index(b: &mut Bencher) {
-        let rocket = rocket();
+        let client = Client::new(rocket()).unwrap();
 
         // Hold all of the requests we're going to make during the benchmark.
         let mut requests = vec![];
-        requests.push(MockRequest::new(Get, "/"));
-        requests.push(MockRequest::new(Put, "/"));
-        requests.push(MockRequest::new(Post, "/"));
+        requests.push(client.get("/"));
+        requests.push(client.put("/"));
+        requests.push(client.post("/"));
 
         b.iter(|| {
             for request in requests.iter_mut() {
-                request.dispatch_with(&rocket);
+                request.mut_dispatch();
             }
         });
     }
 
     #[bench]
     fn bench_dynamic(b: &mut Bencher) {
-        let rocket = rocket();
+        let client = Client::new(rocket()).unwrap();
 
         // Hold all of the requests we're going to make during the benchmark.
         let mut requests = vec![];
-        requests.push(MockRequest::new(Get, "/abc"));
-        requests.push(MockRequest::new(Get, "/abcdefg"));
-        requests.push(MockRequest::new(Get, "/123"));
+        requests.push(client.get("/abc"));
+        requests.push(client.get("/abcdefg"));
+        requests.push(client.get("/123"));
 
         b.iter(|| {
             for request in requests.iter_mut() {
-                request.dispatch_with(&rocket);
+                request.mut_dispatch();
             }
         });
     }
 
     #[bench]
     fn bench_simple_routing(b: &mut Bencher) {
-        let rocket = rocket();
+        let client = Client::new(rocket()).unwrap();
 
         // Hold all of the requests we're going to make during the benchmark.
         let mut requests = vec![];
-        for route in rocket.routes() {
-            let request = MockRequest::new(route.method, route.path.path());
+        for route in client.rocket().routes() {
+            let request = client.req(route.method, route.path.path());
             requests.push(request);
         }
 
         // A few more for the dynamic route.
-        requests.push(MockRequest::new(Get, "/abc"));
-        requests.push(MockRequest::new(Get, "/abcdefg"));
-        requests.push(MockRequest::new(Get, "/123"));
+        requests.push(client.get("/abc"));
+        requests.push(client.get("/abcdefg"));
+        requests.push(client.get("/123"));
 
         b.iter(|| {
             for request in requests.iter_mut() {
-                request.dispatch_with(&rocket);
+                request.mut_dispatch();
             }
         });
     }

DJMcNab/Rocket

commit a63fac7ec2b88b16c0b6c815f22f0bc6af467032

Fix compilation of benchmarks
Cargo was blocking compilation from macros.
```
error[E0658]: `macro` is experimental (see issue #39412)
 --> lib\benches\format-routing.rs:8:1
```
Also fixed warning about unused imports caused by
b8ba7b855f28be1f17bd65b35dd8597d368369ff
As well as unused variable a (fixed by giving an underscore name)

Unless I'm mistaken, these changes should not affect the benchmarks meaningfully

lib/benches/format-routing.rs
@@ -1,4 +1,4 @@
-#![feature(test, plugin)]
+#![feature(test, plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;

lib/benches/ranked-routing.rs
@@ -1,4 +1,4 @@
-#![feature(test, plugin)]
+#![feature(test, plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;

lib/benches/simple-routing.rs
@@ -1,4 +1,4 @@
-#![feature(test, plugin)]
+#![feature(test, plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 // #![feature(alloc_system)]
 // extern crate alloc_system;
@@ -29,8 +29,8 @@ fn index_b() -> &'static str { "index" }
 #[get("/c")]
 fn index_c() -> &'static str { "index" }
 
-#[get("/<a>")]
-fn index_dyn_a(a: &RawStr) -> &'static str { "index" }
+#[get("/<_a>")]
+fn index_dyn_a(_a: &RawStr) -> &'static str { "index" }
 
 fn hello_world_rocket() -> rocket::Rocket {
     let config = Config::new(Environment::Production).unwrap();
@@ -50,7 +50,6 @@ mod benches {
     use super::{hello_world_rocket, rocket};
     use self::test::Bencher;
     use rocket::local::Client;
-    use rocket::http::Method::*;
 
     #[bench]
     fn bench_hello_world(b: &mut Bencher) {